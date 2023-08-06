0b3d0f61ac08caafcfbc62b17ec2c7f227bd7895
jeniva0812 <101515515+jeniva0812@users.noreply.github.com>
2023-08-06 20:07:08 +0530

    Create Type.js

---
+++ Components/Home/Type.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Student",
+          "Coder",
+          "Learner",
+          "Thinker",
+          "Developer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
